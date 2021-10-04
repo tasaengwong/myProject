885786c06d07951643d04b06e127acf13bb00e64
tasaengwong <84965462+tasaengwong@users.noreply.github.com>
2021-10-04 23:05:30 +0700

    add buttonandreport

---
+++ teacher/button.php
@@ -0,0 +1,136 @@
+<!-- Edit -->
+<div class="modal fade " id="edit<?php echo $data['stu_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel">ข้อมูลนิสิต</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+
+            </div>
+            <div class="modal-body  ">
+                <?php
+                $edit = mysqli_query($conn, "select * from students left join company on students.comp_id = company.comp_id where stu_id='" . $data['stu_id'] . "'");
+                $erow = mysqli_fetch_array($edit);
+                ?>
+                <div class="container-fluid">
+                    <form method="POST" action="edit.php?id=<?php echo $erow['stu_id']; ?>">
+                        <div class="row">
+                            <div class="col-lg-10">
+                                <label style="position:relative; top:7px;">รหัสนิสิต:&nbsp;<?php echo $erow['stu_id']; ?></label>
+                            </div>
+                        </div>
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-9 ">
+                                <label>ชื่อ-นามสกุล:&nbsp;<?php echo $erow['name']; ?>&nbsp;<?php echo $erow['lastname']; ?></label>
+                            </div>
+
+                        </div>
+
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-9">
+                                <label>สาขา:&nbsp;<?php echo $erow['major']; ?></label>&nbsp;
+                                <label>ชั้นปี:&nbsp;<?php echo $erow['year']; ?></label>
+                            </div>
+                        </div>
+
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-10">
+                                <label>ที่อยู่:&nbsp;<?php echo $erow['address']; ?></label>
+                            </div>
+
+                        </div>
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-10">
+                                <label>ตำบล:&nbsp;<?php echo $erow['district']; ?></label>
+                                <label>อำเภอ:&nbsp;<?php echo $erow['amphures']; ?></label>
+                                <label>จังหวัด:&nbsp;<?php echo $erow['province']; ?></label>
+
+                            </div>
+
+                        </div>
+
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-9">
+                                <label>รหัสไปรษณีย์:&nbsp;<?php echo $erow['zipcode']; ?></label>
+                            </div>
+
+                        </div>
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label style="position:relative; top:7px;">เบอร์โทรศัพท์:&nbsp;<?php echo $erow['phone']; ?></label>
+                            </div>
+
+                        </div>
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label style="position:relative; top:7px;">E-mail:&nbsp;<?php echo $erow['mail']; ?></label>
+                            </div>
+
+                        </div>
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label style="position:relative; top:7px;">ตำแหน่งงาน:&nbsp;<?php echo $erow['Job']; ?></label>
+                            </div>
+                        </div>
+
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label style="position:relative; top:7px;">รายละเอียด:&nbsp;<?php echo $erow['description']; ?></label>
+                            </div>
+
+                        </div>
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label style="position:relative; top:7px;">สถานประกอบการ:&nbsp;<?php echo $erow['comp_name']; ?></label>
+                            </div>
+                        </div>
+
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label style="position:relative; top:7px;">ที่อยู่สถานประกอบการ:&nbsp;<?php echo $erow['comp_address']; ?></label>
+                            </div>
+                        </div>
+
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label style="position:relative; top:7px;">ผู้ติดต่อ:&nbsp;<?php echo $erow['contract_name']; ?></label>
+                            </div>
+                        </div>
+
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label style="position:relative; top:7px;">การลงทะเบียนเรียน:&nbsp;<?php echo $erow['study']; ?></label>
+                            </div>
+                        </div>
+
+                        <div style="height:10px;"></div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <label style="position:relative; top:7px;">หารจัดส่งเอกสาร:&nbsp;<?php echo $erow['sent']; ?></label>
+                            </div>
+                        </div>
+
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+                <!-- <button type="submit" class="btn btn-warning"><span class="glyphicon glyphicon-check"></span> Save</button> -->
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- /.modal -->
--- teacher/infromation.php
+++ teacher/infromation.php
@@ -21,6 +21,7 @@ if (!$_SESSION['userid']) {
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
+    
   </head>
 
   <body ng-controller="Ctrl">
@@ -130,7 +131,10 @@ if (!$_SESSION['userid']) {
                       ?>
                     </td>
                     </td>
-                    <td></td>
+                    <td>
+                      <a href="#edit<?php echo $data['stu_id']; ?>" data-toggle="modal" class="btn btn-warning"><span class="glyphicon glyphicon-edit"></span> Edit</a>
+                      <?php include('button.php'); ?>
+                    </td>
                   </form>
                   </tr>
                 <?php
@@ -138,29 +142,33 @@ if (!$_SESSION['userid']) {
                 ?>
               </table>
             </table>
-
           </div>
+          <#?php include('add_modal.php'); ?>
         </div>
     </section>
 
+  
+    <!-- Modal -->
+   
 
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.0/angular.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 
-    <script src="./js/stcsrc.js?3"></script>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.0/angular.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      <!-- Option 1: Bootstrap Bundle with Popper -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+      <script src="./js/stcsrc.js?3"></script>
 
 
 

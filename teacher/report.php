e7c9a0010a6fbf14b030c5dc393594d5e14956e7
tasaengwong <akhom.tasaengwong@gail.com>
2021-10-08 19:59:33 +0700

    edit report color

--- teacher/report.php
+++ teacher/report.php
@@ -7,9 +7,10 @@ if (!$_SESSION['userid']) {
 } else {
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Report Data</title>
@@ -22,21 +23,20 @@ if (!$_SESSION['userid']) {
     <link rel="stylesheet" href="./css/style.css?6">
 
     <style>
-    
-        #chart-container {
-            width: 60%;
-           
-            
-        }
-        
+      #chart-container {
+        width: 60%;
+
+
+      }
     </style>
-    
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-warning">
+
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
       <div class="container-fluid">
         <img src="../img/ict.png" alt="logoict" class="img">
-        
+
         <h3>ระบบสารสนเทศการฝึกงาน</h3>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -69,85 +69,98 @@ if (!$_SESSION['userid']) {
     </nav>
 
     <div class="row justify-content-center">
-    <div id="chart-container">
+      <div id="chart-container">
         <canvas id="graphCanvas"></canvas>
-    </div>
+      </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-   <script>
-        $(document).ready(function() {
-            showGraph();
-        });
-        function showGraph(){
-            {
-                $.post("data.php", function(data) {
-                    console.log(data);
-                    let major = [];
-                    let count = [];
-                    // let count1 = [];
-                    for (let i in data) {
-                        
-                        major.push(data[i].major);
-                        count.push(data[i].count);
-                        // count1.push(data[i].count);
-                    }
+    <script>
+      $(document).ready(function() {
+        showGraph();
+      });
 
-                    let chartdata = {
-                        labels: major,
-                        datasets: [
-                          {
-                                label: ['จำนวน'],
-                                
-                                
-                                backgroundColor: [
-                                  
-                                'rgb(255, 92, 88)',
-                            
-                                
-                                ],
-                                //borderColor: '#46d5f1',
-                                hoverBackgroundColor: '#CCCCCC',
-                                hoverBorderColor: '#666666',
-                                data: count
-                        }
-                        ]
-                    };
+      function showGraph() {
+        {
+          $.post("data.php", function(data) {
+            console.log(data);
+            let major = [];
+            let count = [];
+            // let count1 = [];
+            for (let i in data) {
 
-                    let graphTarget = $('#graphCanvas');
-                    let barGraph = new Chart(graphTarget, {
-                      
-                      type: 'bar',
-                      data : chartdata,
-                      options: {
-                        indexAxis: 'y',
-                        // Elements options apply to all of the options unless overridden in a dataset
-                        // In this case, we are setting the border of each horizontal bar to be 2px wide
-                        elements: {
-                          bar: {
-                            borderWidth: 2,
-                          }
-                        },
-                        responsive: true,
-                        plugins: {
-                          legend: {
-                            position: 'right',
-                          },
-                          title: {
-                            display: true,
-                            text: 'กราฟแสดงข้อมูลนิสิต'
-                          }
-                        }
-                      }
-                
-                    })
-                })
+              major.push(data[i].major);
+              count.push(data[i].count);
+              // count1.push(data[i].count);
             }
+
+            let chartdata = {
+              labels: major,
+              datasets: [{
+                label: ['จำนวน'],
+
+
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(255, 205, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                  'rgb(255, 99, 132)',
+                  'rgb(255, 159, 64)',
+                  'rgb(255, 205, 86)',
+                  'rgb(75, 192, 192)',
+                  'rgb(54, 162, 235)',
+                  'rgb(153, 102, 255)',
+                  'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1,
+                //borderColor: '#46d5f1',
+                hoverBackgroundColor: '#CCCCCC',
+                hoverBorderColor: '#666666',
+                data: count
+              }]
+            };
+
+            let graphTarget = $('#graphCanvas');
+            let barGraph = new Chart(graphTarget, {
+
+              type: 'bar',
+              data: chartdata,
+              options: {
+                indexAxis: 'y',
+                // Elements options apply to all of the options unless overridden in a dataset
+                // In this case, we are setting the border of each horizontal bar to be 2px wide
+                elements: {
+                  bar: {
+                    borderWidth: 2,
+                  }
+                },
+                responsive: true,
+                plugins: {
+                  legend: {
+                    position: 'right',
+                  },
+                  title: {
+                    display: true,
+                    text: 'กราฟแสดงข้อมูลนิสิต'
+                  }
+                }
+              }
+
+            })
+          })
         }
+      }
     </script>
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-          
-</body>
-</html>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+  </body>
+
+  </html>
 <?php } ?>

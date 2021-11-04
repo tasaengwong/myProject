2dc55cf906955b583eb5f70268d366b5a7e72297
tasaengwong <akhom.tasaengwong@gail.com>
2021-11-04 23:45:57 +0700

    edit sum asses

--- student/save.php
+++ student/save.php
@@ -8,20 +8,24 @@ $comp_name = $_POST['comp_name'];
 $major = $_POST['major'];
 $es_id1 = $_POST['es_id1'];
 $es_id2 = $_POST['es_id2'];
+$sum1 = $es_id1+ $es_id2;
 $es_id3 = $_POST['es_id3'];
 $es_id4 = $_POST['es_id4'];
 $es_id5 = $_POST['es_id5'];
 $es_id6 = $_POST['es_id6'];
 $es_id7 = $_POST['es_id7'];
+$sum2 = $es_id3+ $es_id4+$es_id5+$es_id6+$es_id7;
 $es_id8 = $_POST['es_id8'];
 $es_id9 = $_POST['es_id9'];
+$sum3 = $es_id8+ $es_id9;
 $es_id10 = $_POST['es_id10'];
 $es_id11 = $_POST['es_id11'];
 $es_id12 = $_POST['es_id12'];
+$sum4 = $es_id10+ $es_id11+ $es_id12;
 $total1 = $es_id1+$es_id2+$es_id3+$es_id4+$es_id5+$es_id6+$es_id7+$es_id8+$es_id9+$es_id10+$es_id11+$es_id12;
 
-$sql = "INSERT INTO asses (stu_id, name, lastname,comp_name,major, es_id1, es_id2, es_id3, es_id4, es_id5, es_id6, es_id7, es_id8, es_id9, es_id10, es_id11, es_id12, total1)
- VALUE  ('$stu_id','$name','$lastname','$comp_name','$major','$es_id1', '$es_id2', '$es_id3', '$es_id4', '$es_id5', '$es_id6', '$es_id7', '$es_id8', '$es_id9', '$es_id10', '$es_id11', '$es_id12', '$total1')";
+$sql = "INSERT INTO asses (stu_id, name, lastname,comp_name,major, es_id1, es_id2, sum1, es_id3, es_id4, es_id5, es_id6, es_id7,sum2, es_id8, es_id9, sum3, es_id10, es_id11, es_id12, sum4, total1)
+ VALUE  ('$stu_id','$name','$lastname','$comp_name','$major','$es_id1', '$es_id2',$sum1, '$es_id3', '$es_id4', '$es_id5', '$es_id6', '$es_id7', $sum2,'$es_id8', '$es_id9',$sum3, '$es_id10', '$es_id11', '$es_id12',$sum4, '$total1')";
 
 
 $result = mysqli_query($conn, $sql);

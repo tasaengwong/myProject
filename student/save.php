9a3adc651053bcb4fe8bc7d6d3c219fafc218bf1
tasaengwong <akhom.tasaengwong@gail.com>
2021-11-04 23:29:38 +0700

    edit sum asses

--- student/save.php
+++ student/save.php
@@ -18,10 +18,10 @@ $es_id9 = $_POST['es_id9'];
 $es_id10 = $_POST['es_id10'];
 $es_id11 = $_POST['es_id11'];
 $es_id12 = $_POST['es_id12'];
+$total1 = $es_id1+$es_id2+$es_id3+$es_id4+$es_id5+$es_id6+$es_id7+$es_id8+$es_id9+$es_id10+$es_id11+$es_id12;
 
-$sql = "INSERT INTO asses (stu_id, name, lastname,comp_name,major, es_id1, es_id2, es_id3, es_id4, es_id5, es_id6, es_id7, es_id8, es_id9, es_id10, es_id11, es_id12)
- VALUE  ('$stu_id','$name','$lastname','$comp_name','$major','$es_id1', '$es_id2', '$es_id3', '$es_id4', '$es_id5', '$es_id6', '$es_id7', '$es_id8', '$es_id9', '$es_id10', '$es_id11', '$es_id12')";
-
+$sql = "INSERT INTO asses (stu_id, name, lastname,comp_name,major, es_id1, es_id2, es_id3, es_id4, es_id5, es_id6, es_id7, es_id8, es_id9, es_id10, es_id11, es_id12, total1)
+ VALUE  ('$stu_id','$name','$lastname','$comp_name','$major','$es_id1', '$es_id2', '$es_id3', '$es_id4', '$es_id5', '$es_id6', '$es_id7', '$es_id8', '$es_id9', '$es_id10', '$es_id11', '$es_id12', '$total1')";
 
 
 $result = mysqli_query($conn, $sql);

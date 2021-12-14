36db675396f61a42f9269ba04d9612ba223db074
tasengwong <akhom.tasaengwong@gmail.com>
2021-12-14 15:13:45 +0700

    update fileconection

--- admin/connection.php
+++ admin/connection.php
@@ -1,7 +1,8 @@
 <?php
  
 //MySQLi Procedural
-$con = mysqli_connect("localhost","root","","project103");
+
+$con = mysqli_connect("localhost", "trainingsys", "trainingsysYaM5N756h8z5kaQZ", "training");
 if (!$con) {
 	die("Connection failed: " . mysqli_connect_error());
 }
